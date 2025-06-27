ecb472ad208be43205984c1c2b17136fa56e0672
Guilherme Ryam Guedes <125148012+guedesguilherme@users.noreply.github.com>
2025-06-27 12:22:18 -0300

    Update googleDrive.js

--- utils/googleDrive.js
+++ utils/googleDrive.js
@@ -1,11 +1,10 @@
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 const { google } = require('googleapis');
 
 // Caminho temporário para salvar o arquivo JSON (em tempo de execução)
 const tempCredentialsPath = path.join(__dirname, '../temp-credentials.json');
 
-// Se ainda não existir o arquivo, cria a partir da variável de ambiente
 if (!fs.existsSync(tempCredentialsPath)) {
   const credentialsString = process.env.GOOGLE_CREDENTIALS_JSON;
   if (!credentialsString) {
@@ -14,10 +13,9 @@ if (!fs.existsSync(tempCredentialsPath)) {
   fs.writeFileSync(tempCredentialsPath, credentialsString);
 }
 
-
 // Autenticação com conta de serviço
 const auth = new google.auth.GoogleAuth({
-  keyFile: tempCredentialsPath, // caminho para suas credenciais
+  keyFile: tempCredentialsPath,
   scopes: ['https://www.googleapis.com/auth/drive'],
 });
 
@@ -26,8 +24,8 @@ const uploadToDrive = async (filePath, fileName, mimeType) => {
   const drive = google.drive({ version: 'v3', auth: client });
 
   const fileMetadata = {
-    name: fileName, 
-    parents: ['1Z8d6i1lA2vs7ex2FOyvqd4zSl08P2ZQe'], // coloque o ID da pasta onde deseja salvar no Drive
+    name: fileName,
+    parents: ['1Z8d6i1lA2vs7ex2FOyvqd4zSl08P2ZQe'], // ID da pasta no Drive
   };
 
   const media = {
@@ -35,23 +33,34 @@ const uploadToDrive = async (filePath, fileName, mimeType) => {
     body: fs.createReadStream(filePath),
   };
 
-  const response = await drive.files.create({
-    resource: fileMetadata,
-    media,
-    fields: 'id',
-  });
+  try {
+    const response = await drive.files.create({
+      resource: fileMetadata,
+      media,
+      fields: 'id',
+    });
 
-  // Tornar o arquivo público
-  await drive.permissions.create({
-    fileId: response.data.id,
-    requestBody: {
-      role: 'reader',
-      type: 'anyone',
-    },
-  });
+    // Tornar o arquivo público
+    await drive.permissions.create({
+      fileId: response.data.id,
+      requestBody: {
+        role: 'reader',
+        type: 'anyone',
+      },
+    });
 
-  const fileUrl = `https://drive.google.com/uc?id=${response.data.id}`;
-  return fileUrl;
+    const fileUrl = `https://drive.google.com/uc?id=${response.data.id}`;
+
+    // ✅ Remover o arquivo local APÓS o upload e permissão concluídos
+    fs.unlink(filePath, (err) => {
+      if (err) console.error(`Erro ao deletar arquivo local ${filePath}:`, err);
+    });
+
+    return fileUrl;
+  } catch (err) {
+    console.error('Erro no upload para o Google Drive:', err);
+    throw err;
+  }
 };
 
 module.exports = uploadToDrive;
